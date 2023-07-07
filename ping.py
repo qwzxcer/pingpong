466a34f160ca5d9d19f73582bfe597bb4241b551
qwzxcer <138557886+qwzxcer@users.noreply.github.com>
2023-07-07 15:24:06 +0600

    complete version ping.py
    v1.2

--- ping.py
+++ ping.py
@@ -28,6 +28,18 @@ class GameSprite(sprite.Sprite):
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
 
+class GameSprite1(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, player_speed_x,player_speed_y, height, weight):
+        super().__init__()
+        self.image=transform.scale(image.load(player_image), (height,weight))
+        self.speed_x=player_speed_x
+        self.speed_y=player_speed_y
+        self.rect = self.image.get_rect()
+        self.rect.x=player_x
+        self.rect.y=player_y
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
 class Player1(GameSprite):
     def update(self):
             global a
@@ -48,20 +60,23 @@ class Player2(GameSprite):
                 if keys_pressed[K_DOWN] and self.rect.y <395:
                     self.rect.y +=self.speed
 
-class Ball(GameSprite):
+class Ball(GameSprite1):
     def update(self):
-        self.rect.x+=self.speed
-        if self.rect.x>800 or self.rect.x<-100 or self.rect.y>600 or self.rect.y<-100:
+        self.rect.x+=self.speed_x
+        self.rect.y+=self.speed_y
+        if self.rect.x>710 or self.rect.x<-10:
             self.kill()
 
         
 
 player1=Player1('paddle.png', x1,y1,3,30,100)
 player2=Player2('paddle.png', x2,y2,3,30,100)
-ball=Ball('ball.png', 350,250,5,40,40)
+player1.rect.x+=3
+ball=Ball('ball.png', 350,250,5,5,40,40)
 game=True
 finish=False
 wait=120
+wait1=0
 while game:
     for e in event.get():
         if e.type == QUIT:
@@ -74,18 +89,24 @@ while game:
     ball.reset()
     ball.update()
     if finish == False:
-        if sprite.collide_rect(ball, player2):
-            ball.rect.x -=ball.speed
-        if sprite.collide_rect(ball, player1):
-            ball.rect.x +=ball.speed
+        if ball.rect.y>460 or ball.rect.y<=0:
+            ball.speed_y*=-1
+        if sprite.collide_rect(player1, ball) or sprite.collide_rect(player2, ball):
+            if wait1<=0:
+                ball.speed_x*=-1
+                wait1=15
+        if wait1 != 0:
+            wait1-=1
     if ball.rect.x>690:
         finish=True
         a=False
         window.blit(win1,(200,200))
         if wait==0:
-            wait==120
+            wait=120
             finish=False
             a=True
+            ball.rect.x=350
+            ball.rect.y=250
         else:
             wait-=1
     if ball.rect.x<10:
@@ -93,11 +114,14 @@ while game:
         window.blit(win2,(200,200))
         a=False 
         if wait==0:
-            wait==120
+            wait=120
+            
             finish=False
             a=True
+            ball.rect.x=350
+            ball.rect.y=250
         else:
-            wait-=1  
+            wait-=1
     display.update()
     clock.tick(FPS)
         
